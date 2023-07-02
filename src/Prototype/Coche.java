69276bb36a5120d22b5202a3579dc85d057fd527
TylizRv <caosavenger@gmail.com>
2023-07-02 23:53:44 +0100

    Clase Prototype

---
+++ src/Prototype/Coche.java
@@ -0,0 +1,18 @@
+package Prototype;
+
+public class Coche implements Cloneable {
+    String marca;
+    String modelo;
+    int puertas;
+
+    public Coche() {}
+
+    public Coche clonar() throws CloneNotSupportedException {
+        return (Coche)this.clone();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{marca: %s, modelo: %s, puertas: %d}", marca, modelo, puertas);
+    }
+}
